08dc6708f086274091189599ef306b29549ab8b8
Ilya4rh <chernykn12ilya12@gmail.com>
2024-12-25 14:02:36 +0300

    Добавил интерфейс и класс для генерации цвета

---
+++ TagsCloudVisualization/ColorGenerator/ColorGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using TagsCloudVisualization.Settings;
+
+namespace TagsCloudVisualization.ColorGenerator;
+
+public class ColorGenerator : IColorGenerator
+{
+    private readonly ColorGeneratorSettings colorGeneratorSettings;
+    private readonly Random random;
+    
+    public ColorGenerator(ColorGeneratorSettings colorGeneratorSettings)
+    {
+        this.colorGeneratorSettings = colorGeneratorSettings;
+        random = new Random();
+    }
+    
+    public Color GetColor()
+    {
+        if (colorGeneratorSettings.ColorName == "random")
+        {
+            return Color.FromArgb(random.Next(255), random.Next(255), random.Next(255));
+        }
+        
+        return Color.FromName(colorGeneratorSettings.ColorName);
+    }
+}
