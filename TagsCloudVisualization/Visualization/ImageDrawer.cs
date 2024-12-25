2f7958ed5a8697dd0429896bf415fb9d39e3aa89
Ilya4rh <chernykn12ilya12@gmail.com>
2024-12-25 14:03:41 +0300

    Реализовал ImageDrawer

--- TagsCloudVisualization/Visualization/ImageDrawer.cs
+++ TagsCloudVisualization/Visualization/ImageDrawer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using TagsCloudVisualization.ColorGenerator;
 using TagsCloudVisualization.Settings;
 using TagsCloudVisualization.Tags;
 
@@ -7,14 +8,32 @@ namespace TagsCloudVisualization.Visualization;
 public class ImageDrawer : IImageDrawer
 {
     private readonly ImageSettings imageSettings;
+    private readonly BackgroundSettings backgroundSettings;
+    private readonly IColorGenerator colorGenerator;
 
-    public ImageDrawer(ImageSettings imageSettings)
+    public ImageDrawer(
+        ImageSettings imageSettings, 
+        BackgroundSettings backgroundSettings, 
+        IColorGenerator colorGenerator)
     {
         this.imageSettings = imageSettings;
+        this.backgroundSettings = backgroundSettings;
+        this.colorGenerator = colorGenerator;
     }
 
     public Bitmap Draw(IEnumerable<Tag> tags)
     {
-        throw new NotImplementedException();
+        var bitmap = new Bitmap(imageSettings.Width, imageSettings.Height);
+        var graphics = Graphics.FromImage(bitmap);
+        graphics.Clear(backgroundSettings.BackgroundColor);
+        
+        foreach (var tag in tags)
+        {
+            graphics.DrawString(tag.Content, 
+                new Font(tag.Font, tag.Size), 
+                new SolidBrush(colorGenerator.GetColor()),
+                tag.Rectangle);
+        }
+        return bitmap;
     }
 }
