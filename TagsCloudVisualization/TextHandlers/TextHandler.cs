f3e92f8d1a5d30b7c54f4958d582e6df8fa002d5
Ilya4rh <chernykn12ilya12@gmail.com>
2024-12-25 13:13:35 +0300

    Реализовал обработчик текста

--- TagsCloudVisualization/TextHandlers/TextHandler.cs
+++ TagsCloudVisualization/TextHandlers/TextHandler.cs
@@ -1,18 +1,52 @@
 ﻿using TagsCloudVisualization.FileReaders;
+using TagsCloudVisualization.Settings;
 
 namespace TagsCloudVisualization.TextHandlers;
 
 public class TextHandler : ITextHandler
 {
-    private readonly IFileReader fileReader;
+    private readonly IEnumerable<string> words;
+    private readonly HashSet<string> boringWords;
+    private readonly IFileReader[] readers;
 
-    public TextHandler(IFileReader fileReader)
+    public TextHandler(IFileReader[] readers, TextHandlerSettings settings)
     {
-        this.fileReader = fileReader;
+        this.readers = readers;
+        var reader = GetReader(settings.PathToText);
+
+        words = reader.Read(settings.PathToText);
+        
+        var boringWordsReader = GetReader(settings.PathToBoringWords);
+
+        boringWords = boringWordsReader.Read(settings.PathToBoringWords).ToHashSet();
     }
 
     public Dictionary<string, int> GetWordsCount()
     {
-        throw new NotImplementedException();
+        var wordsCount = new Dictionary<string, int>();
+
+        foreach (var word in words)
+        {
+            var lowerWord = word.ToLower();
+            
+            if (wordsCount.TryGetValue(lowerWord, out var value))
+            {
+                wordsCount[lowerWord] = ++value;
+                continue;
+            }
+            if (!boringWords.Contains(lowerWord))
+            {
+                wordsCount[lowerWord] = 1;
+            }
+        }
+
+        return wordsCount;
+    }
+    
+    private IFileReader GetReader(string path)
+    {
+        var reader = readers.FirstOrDefault(r => r.CanRead(path));
+        
+        return reader ?? throw new ArgumentException($"Can't read file with path {path}");
     }
 }
