e90d4b59348bfe52fc069f7295f68ffddad010e6
Ilya4rh <chernykn12ilya12@gmail.com>
2024-12-25 13:54:50 +0300

    Добавил класс настройки цветов

---
+++ TagsCloudVisualization/Settings/ColorGeneratorSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace TagsCloudVisualization.Settings;
+
+public class ColorGeneratorSettings
+{
+    public ColorGeneratorSettings(string colorName)
+    {
+        ColorName = colorName;
+    }
+
+    public string ColorName { get; }
+}
